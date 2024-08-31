33975000bc127337bf9976e715d31ae4a304da68
joaquim6399 <joaquim6399@gmail.com>
2024-08-31 16:47:35 +0100

    Added Game class to represent each game

--- Models/Game.cs
+++ Models/Game.cs
@@ -2,5 +2,13 @@ namespace GameCatalog.Models;
 
 public class Game
 {
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string ImageIconUrl { get; set; }
+    public double CriticScore { get; set; }
+    public double PersonalScore { get; set; }
+    public string PersonalNotes { get; set; }
     
 }
